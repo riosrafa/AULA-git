eb154d0364af688a7b4e50a17a0a3b93fc0c5817
Rafael rios <rafaelmaiarios@hotmail.com>
2019-08-30 00:26:51 -0300

    Testando-Aprendendo, projeto github

---
+++ src/course/Principal.java
@@ -0,0 +1,18 @@
+package course;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		String n = sc.next();
+		System.out.println("Teste : "+ n);
+		
+		sc.close();
+	}
+
+}
